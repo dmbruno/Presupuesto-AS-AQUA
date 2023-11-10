4f1387d5e49414acbd18f4d889a69909bdeac906
diego <dmbruno61@gmail.com>
2023-11-10 16:04:48 -0300

    fix DOM

--- js/java.js
+++ js/java.js
@@ -1,67 +1,144 @@
-
-
-let presupuesto = 0;
-const calculoP = document.querySelector('.btn-calcular')
-calculoP.addEventListener('click', (event) => {
+const iniciar = document.querySelector('.encabezado')
+iniciar.addEventListener('click', (event) => {
     event.preventDefault();
-    const longitud = document.querySelector('.longitud');
-    const CantLongitud = parseFloat(longitud.value);
-    const aspersor = document.querySelector('.aspersor');
-    const CantAspersor = parseFloat(aspersor.value);
-    const acople = document.querySelector('.acople');
-    const CantAcople = parseFloat(acople.value);
-    const longitudC = document.querySelector('.tuberiaC');
-    const valLongitud = parseFloat(longitudC.value);
-    const aspersorC = document.querySelector('.aspersorC');
-    const valAspersor = parseFloat(aspersorC.value);
-    const acopleC = document.querySelector('.acopleC');
-    const valAcople = parseFloat(acopleC.value);
-    const horas = document.querySelector('.CantHoras');
-    const cantHoras = parseFloat(horas.value);
-    const horasT = document.querySelector('.horasT');
-    const tarifaH = parseFloat(horasT.value);
-    const zona = document.querySelector('.colectorC')
-    const colector = parseFloat(zona.value)
-    const zonasR = document.querySelector('.zonasR')
-    const cantZonas = parseFloat(zonasR.value)
 
-    presupuesto = parseFloat(CantLongitud * valLongitud + 
-        CantAspersor * valAspersor +  
-        cantHoras * tarifaH + colector * cantZonas );
+    const formularioCostos = document.createElement('div')
+    formularioCostos.innerHTML =
+        `
+        <form>
+            <label for="costoTuberia">Costo de tubería (por metro):</label><br>
+            <input type="number" class="costoTuberia" name="costoTuberia"><br>
+            <label for="costoAspersor">Costo por aspersor (por unidad):</label><br>
+            <input type="number" class="costoAspersor" name="costoAspersor"><br>
+            <label for="costoSelector">Costo por selector:</label><br>
+            <input type="number" class="costoSelector" name="costoSelector"><br>
+            <label for="tarifaTrabajo">Tarifa por horas de trabajo:</label><br>
+            <input type="number" class="tarifaTrabajo" name="tarifaTrabajo"><br>
+            <input type="submit" value="Enviar" class="btn-enviar">
+    </form>
+        `
 
-    const card = document.createElement('div');
-    card.classList.add('card');
 
-    const titularProyecto = document.querySelector('.titular')
-    const usuario = titularProyecto.value;
+    document.body.appendChild(formularioCostos)
 
-    const direccionProyecto = document.querySelector('.direccion');
-    const hubicacion = direccionProyecto.value;
 
-    card.innerHTML = `
-                <h1>Cliente ${usuario}</h1>
-                <h3>Direccion: ${hubicacion}</h3>
-                <hr>  <!-- Línea horizontal -->
-                <p>Longitud de tuberías: ${CantLongitud} metros</p>
-                <p>Número de aspersores: ${CantAspersor}</p>
-                <p>Número de acoples: ${CantAcople}</p>
-                <p>Costo por metro de tubería: ${valLongitud}</p>
-                <p>Costo por aspersor: ${valAspersor}</p>
-                <p>Costo por acople: ${valAcople}</p>
-                <p>Horas de trabajo estimadas: ${cantHoras}</p>
-                <p>Tarifa por hora de trabajo: ${tarifaH}</p>
-                <hr>  <!-- Línea horizontal -->
-                <h2>Presupuesto total: ${presupuesto}</h2>
-                    <div class="contenedor-aclaracion">
-                    <p class="aclaracion">*Todos los presupuesto tienen validez por 15 dias corridos desde la fecha de presentacion del mismo. El precio del presupuesto esta expresador en USD blue, podes pagar con pesos al cambio del dia </p>
-                    </div>
-                    `;
+    const formularioP = document.querySelector('.btn-enviar')
+    formularioP.addEventListener('click', (event) => {
+        event.preventDefault();
+        const costoTuberias = document.querySelector('.costoTuberia')
+        const costoTub = costoTuberias.value
 
-    document.body.appendChild(card);
-    const formulario = document.querySelector('.contenedor-formulario');
-    formulario.remove();
+        const costoAspersores = document.querySelector('.costoAspersor')
+        const costoAspersor = costoAspersores.value
 
-});
+        const costoSelectores = document.querySelector('.costoSelector')
+        const costoSelector = costoSelectores.value
+
+        const tarifaHoras = document.querySelector('.tarifaTrabajo')
+        const costoHoras = tarifaHoras.value
+        const presupuesto = document.createElement('div')
+
+        presupuesto.innerHTML =
+            `
+        <form action="">
+        <label for="fecha">Fecha:</label><br>
+        <input type="date" id="fecha" name="fecha"><br>
+        <label for="cliente">Cliente:</label><br>
+        <input type="text" id="cliente" name="cliente"><br>
+        <label for="domicilio">Domicilio:</label><br>
+        <input type="text" id="domicilio" name="domicilio"><br>
+        <label for="telefono">Teléfono:</label><br>
+        <input type="tel" id="telefono" name="telefono"><br>
+        <label for="metros">Cantidad de metros a regar:</label><br>
+        <input type="number" id="metros" name="metros"><br>
+        <label for="zonas">Cantidad de zonas a regar:</label><br>
+        <input type="number" id="zonas" name="zonas"><br>
+        <label for="aspersores">Cantidad de aspersores:</label><br>
+        <input type="number" id="aspersores" name="aspersores"><br>
+        <label for="tuberias">Cantidad de tuberías:</label><br>
+        <input type="number" id="tuberias" name="tuberias"><br>
+        <label for="horas">Horas:</label><br>
+        <input type="number" id="horas" name="horas"><br>
+        <h3>Opciones:</h3>
+        <input type="checkbox" id="bomba" name="bomba">
+        <label for="bomba">Bomba</label><br>
+        <input type="checkbox" id="tableroelectrico" name="tableroelectrico">
+        <label for="tableroelectrico">Tablero Eléctrico</label><br>
+        <input type="checkbox" id="tablerowifi" name="tablerowifi">
+        <label for="tablerowifi">Tablero Wi-Fi</label><br><br>
+        <input type="submit" value="Calcular Presupuesto" class="calculo">
+        </form> 
+    
+
+        `
+        document.body.appendChild(presupuesto)
+        formularioCostos.remove();
+        
+        const calculo = document.querySelector('.calculo')
+        calculo.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const date = document.querySelector('#fecha')
+            const fecha = date.value
+            const user = document.querySelector('#cliente')
+            const cliente = user.value
+            const adress = document.querySelector('#domicilio')
+            const domicilio = adress.value
+            const celphone = document.querySelector('#telefono')
+            const contacto = celphone.value
+            const metrosa = document.querySelector('#metros')
+            const aRegar = metrosa.value
+            const zones = document.querySelector('#zonas')
+            const sectores = zones.value
+            const asper = document.querySelector('#aspersores')
+            const cantAspersores = asper.value
+            const mangueras = document.querySelector('#tuberias')
+            const cantMangueras = mangueras.value
+            const hours = document.querySelector('#horas')
+            const CantHoras = hours.value
+
+            const resultado = aRegar * costoTub + sectores * costoSelector + cantAspersores * costoAspersor + CantHoras * costoHoras
+            
+            const presupuestoF = document.createElement('div')
+            presupuestoF.classList.add('contenedorPresupuesto')
+            presupuestoF.innerHTML=
+             `
+            <div class="contenedorPres">
+                <h2 class="tituloPres">Presupuesto</h2><hr>
+                <li class="fechaPres">Fecha: ${fecha}</li>
+                <li class="clientePres">Cliente: ${cliente}</li>
+                <li class="domicilioPres">Domicilio: ${domicilio}</li>
+                <li class="contactoPres">Contacto: ${contacto}</li>
+                <li class="metrosPres">Metros a regar: ${aRegar}</li>
+                <li class="sectoresPres">Sectores: ${sectores}</li>
+                <li class="cantidadPres">Cantidad de aspersores: ${cantAspersores}</li>
+                <li class="manguerasPres">Cantidad de mangueras: ${cantMangueras}</li>
+                <li class="horasPres">Horas: ${CantHoras}</li>
+                <h3 class="totalPres">Total: ${resultado}</h3>
+            </div>
+            ` 
+            document.body.appendChild(presupuestoF)
+            presupuesto.remove();
+
+
+        })
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
